2cef625ab1c9367f4adb9e4d5d278cf9b556c88b
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-05-28 11:49:15 -0400

    Update build.rs
    Added Nova's Run to the build script.

--- build.rs
+++ build.rs
@@ -18,6 +18,18 @@ fn main()
 		.output()
 		.expect("Failed to execute Stylus script");
 	
+	
+	Command::new(program)
+		.args(&[firstArg, "npm run build --prefix ./subprojects/novas-run"])
+		.output()
+		.expect("Failed to build subproject");
+	
 	//Just always re-run this script
 	println!("cargo:rerun-if-changed=stylus/**/*.styl");
+	
+	println!("cargo:rerun-if-changed=subprojects/novas-run/public/**/*.*");
+	println!("cargo:rerun-if-changed=subprojects/novas-run/src/**/*.*");
+	println!("cargo:rerun-if-changed=subprojects/novas-run/next.config.js");
+	println!("cargo:rerun-if-changed=subprojects/novas-run/package.json");
+	println!("cargo:rerun-if-changed=subprojects/novas-run/tsconfig.json");
 }
