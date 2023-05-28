565fcf33106abc6661d0e25947efcaccb1a4c4dd
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-05-28 11:53:06 -0400

    Update build.rs

--- build.rs
+++ build.rs
@@ -27,9 +27,6 @@ fn main()
 	//Just always re-run this script
 	println!("cargo:rerun-if-changed=stylus/**/*.styl");
 	
-	println!("cargo:rerun-if-changed=subprojects/novas-run/public/**/*.*");
-	println!("cargo:rerun-if-changed=subprojects/novas-run/src/**/*.*");
-	println!("cargo:rerun-if-changed=subprojects/novas-run/next.config.js");
-	println!("cargo:rerun-if-changed=subprojects/novas-run/package.json");
-	println!("cargo:rerun-if-changed=subprojects/novas-run/tsconfig.json");
+	["public/**/*.*", "src/**/*.*", "next.config.js", "package.json", "tsconfig.json"]
+		.map(|frag| println!("cargo:rerun-if-changed=subprojects/novas-run/{}", frag));
 }
