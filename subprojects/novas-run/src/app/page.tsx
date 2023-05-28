fd6cb9695a2094c3924e4b46ec2467f800e065ce
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-05-28 16:56:01 -0400

    First Draft

--- subprojects/novas-run/src/app/page.tsx
+++ subprojects/novas-run/src/app/page.tsx
@@ -9,8 +9,21 @@ export default function Home()
 		<main className={styles.main}>
 			<h1>Nova's Run</h1>
 			
+			<p className={styles.description}>
+				Nova's Run is a simple 2D platformer where you play as Nova, a curious
+				and adorable little house cat who wants to explore the world. It was
+				built with React/Next.js and is rendered in an HTML Canvas element.
+			</p>
+			
 			<div className={styles.game}>
 				<Game></Game>
+				<div className={styles.gameControls}>
+					<span><label>Walk Left:</label> A ⬅</span>
+					<span><label>Jump:</label> W ⬆</span>
+					<span><label>Walk Right:</label> D ➡</span>
+					<span><label>Sneak:</label> S ⬇</span>
+					<span><label>Run:</label> V 0</span>
+				</div>
 			</div>
 			
 			<div className={styles.attributions}>
@@ -19,6 +32,8 @@ export default function Home()
 					<li>Animated Cat Sprite: <a href="https://opengameart.org/content/gudejump-art-pack">Gude!Jump Art Pack</a></li>
 				</ul>
 			</div>
+			
+			<div className={styles.footer}>© 2023 Peter Lunneberg</div>
 		</main>
 	)
 }
