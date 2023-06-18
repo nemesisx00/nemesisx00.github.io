693e8eb6d16a19376a42e962bf2bf9654c9eca9c
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-06-18 14:24:10 -0400

    Updated Body Content
    Updated headings and wording of the content.
    Updated styles.

--- src/components/body.rs
+++ src/components/body.rs
@@ -29,34 +29,44 @@ fn Collaborate(cx: Scope, lookingForWork: bool) -> Element
 	{
 		section
 		{
-			h1 { "Want to Collaborate?" }
+			h1 { "Want To Collaborate With Me?" }
 			
 			p
 			{
-				r#"I am available to provide code review and tutoring services, likely
-				for a modest fee. I would also love to contribute to your open source
-				project."#
+				r#"Are you stuck on a task? Is there an algorithm or software
+				design pattern that you would like to understand better? Or
+				maybe you are looking to put together a team for a new project."#
+			}
+			
+			p
+			{
+				r#"Let me help! I am available to provide code review and tutoring
+				services, as well as contribute to open source projects that can
+				capture my interest. Let's have a conversation about the details
+				of your particular needs."#
 			}
 			
 			(lookingForWork).then(move || rsx!(p
 			{
-				r#"I am currently available for employment. If you're looking for a highly
-				skilled and adaptable full stack software developer, send me a description
-				of your company or project and the position you want to fill. I am only
-				considering fully remote positions at this time."#
+				r#"I am also currently looking for employment. If you could use
+				a seasoned, highly skilled, and adaptable full stack software
+				developer, send me a description of the position you are looking
+				to fill. Note: I am only considering fully remote positions at
+				this time."#
 			}))
 			
 			p
 			{
-				r#"I'm also open to collaboration on things other than software development.
-				For example, if you're putting together a TTRPG Actual Play series, I
-				don't think you would be surprised to learn that, like most enthusiasts,
-				I have created many more character concepts than I have had the opportunity
-				to play."#
+				r#"In any case, if you're interested in working with me, feel
+				free to reach out!"#
 			}
 			
-			p { "In any case, if you're interested in working with me, feel free to reach out!" }
-			a { class: "button", href: "mailto:plunn.collab@gmail.com", "Contact Me" }
+			a
+			{
+				class: "button",
+				href: "mailto:plunn.collab@gmail.com",
+				"Reach Out"
+			}
 		}
 	});
 }
@@ -69,37 +79,42 @@ fn GeneralistPolyglot(cx: Scope) -> Element
 	{
 		section
 		{
-			h1 { "What is a Generalist Polyglot Developer?" }
+			h1 { "What Do I Mean By Generalist Polyglot Developer?" }
 			
 			p
 			{
 				r#"I consider myself to be a polyglot. I'm not referring to my
-				conversational fluency in Norwegian, my having dabbled in 10 or more
-				other spoken languages, nor my ability to read several non-Latin writing
-				systems phonetically."#
+				conversational fluency in Norwegian, my having dabbled in 10 or
+				more other spoken languages, nor my ability to read several
+				non-Latin writing systems phonetically."#
 			}
 			
 			p
 			{
-				r#"A programming language is just that: a language. As I learn a new
-				language, I intuitively bring over understanding from all the other
-				languages I already know, dramatically reducing the time needed to attain
-				fluency."#
+				r#"A programming language is just that: a language. It is a set
+				of grammar and syntax which provides a means of communicating
+				with a computer. As with spoken language, when I learn a new
+				programming language I intuitively bring over understanding from
+				all the other languages with which I am familiar, dramatically
+				reducing the time needed to attain fluency."#
 			}
 			
 			p
 			{
-				r#"This is related to why I refer to myself as an intentional generalist.
-				I am practiced in rapidly assimilating new information and learning to
-				use new technologies. Leveraging my more than 15 years of experience
-				learning new things, as opposed to requiring explicit training, I can get
-				up to speed on a completely new-to-me technology in a day or two, at most."#
+				r#"Similarly to learning a language, gaining proficiency with
+				any new technology is a process which becomes easier the more
+				you do it. Over the course of my career as a software developer
+				I have become exceedingly efficient at this process, to the
+				point of being able to start working with a completely new-to-me
+				technology almost immediately. This is why I began referring to
+				myself as an intentional generalist."#
 			}
 			
 			p
 			{
-				r#"The result of all of this is that I know I can work with any software
-				development technology regardless of whether or not I've used it before."#
+				r#"As a result of all of this, I know I can work with any software
+				development technology regardless of whether or not I've used it
+				before."#
 			}
 		}
 	});
@@ -115,32 +130,36 @@ fn WhoAmI(cx: Scope) -> Element
 		{
 			h1 { "Who Am I?" }
 			
-			p { "I'm a gamer, software developer, and language enthusiast. Or, in a word, a nerd." }
+			p
+			{
+				r#"I'm a gamer, software developer, and language enthusiast. Or,
+				in a word, a nerd."#
+			}
 			
 			p
 			{
-				r#"I started working as a professional software developer after graduating
-				from university back in 2008. Over the course of my career, I've designed,
-				implemented, and maintained software applications using a myriad of frontend,
-				backend, and database technologies."#
+				r#"I started working as a professional software developer after
+				graduating from university in 2008. Over the course of my career,
+				I've designed, implemented, and maintained software applications
+				using a myriad of frontend, backend, and database technologies."#
 			}
 			
 			p
 			{
 				r#"On my own time, I prefer to explore areas of software development
-				that are outside the domains of my professional work. These have often
-				skewed towards desktop GUI applications and game development. I also
-				enjoy trying out new, or new-to-me, technologies. After all, there is
-				always more to learn."#
+				that are outside the domains of my professional work. Most notably,
+				I have a passion for game development. I also enjoy trying out
+				new, or new-to-me, technologies. After all, there is always more
+				to learn."#
 			}
 			
 			p
 			{
-				r#"However, everyone needs to take a step back from the grind and relax
-				from time to time. I prefer immersing myself in engaging stories and
-				interactive storytelling. This can range from watching movies or TV
-				shows to joining a weekly session playing collaborative storytelling
-				games, more commonly known as TTRPGs."#
+				r#"However, everyone needs to take a step back from the grind
+				and relax from time to time. I prefer immersing myself in engaging
+				stories and interactive storytelling, whether in the form of
+				movies or TV shows, video games, or collaborative storytelling
+				sessions (i.e. TTRPGs)."#
 			}
 		}
 	});
