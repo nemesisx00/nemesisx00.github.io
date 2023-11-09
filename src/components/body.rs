942318aeffda6505cb54963073e8bd73396c6868
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-09 17:47:37 -0500

    Update body.rs
    Updated use statement to use the convention of starting with :: for non-std external imports.
    
    Reworked wordings for WhoAmI and Collaborate.
    
    Replaced GeneralistPolyglot component with new Credentials component.
    
    Added class name to the p element rendered when lookingForWork is true, to apply additional styles.

--- src/components/body.rs
+++ src/components/body.rs
@@ -1,7 +1,7 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
-use dioxus::prelude::*;
+use ::dioxus::prelude::*;
 
 pub fn BodyContent(cx: Scope) -> Element
 {
@@ -13,7 +13,7 @@ pub fn BodyContent(cx: Scope) -> Element
 			
 			WhoAmI {}
 			hr {}
-			GeneralistPolyglot {}
+			Credentials {}
 			hr {}
 			Collaborate { lookingForWork: true }
 		}
@@ -22,6 +22,9 @@ pub fn BodyContent(cx: Scope) -> Element
 
 // --------------------------------------------------
 
+/**
+Call to action.
+*/
 #[inline_props]
 fn Collaborate(cx: Scope, lookingForWork: bool) -> Element
 {
@@ -29,27 +32,35 @@ fn Collaborate(cx: Scope, lookingForWork: bool) -> Element
 	{
 		section
 		{
-			h1 { "Want To Collaborate With Me?" }
+			h1 { "WANT TO COLLABORATE?" }
 			
 			p
 			{
 				r#"Are you stuck on a task? Is there an algorithm or software
 				design pattern that you would like to understand better? Or
-				maybe you are looking to put together a team for a new project."#
+				maybe you are looking to put together a team for a new project.
+				I'm here to help! I can provide many software development
+				services, including but not limited to:"#
+			}
+			
+			ul
+			{
+				li { "Code Review" }
+				li { "Design Consultation" }
+				li { "Development Commissions" }
 			}
 			
 			p
 			{
-				r#"Let me help! I am available to provide code review and tutoring
-				services, as well as contribute to open source projects that can
-				capture my interest. Let's have a conversation about the details
-				of your particular needs."#
+				r#"Let's have a conversation about your particular needs."#
 			}
 			
-			(lookingForWork).then(move || rsx!(p
+			lookingForWork.then(move || rsx!(p
 			{
-				r#"I am also currently looking for employment. If you could use
-				a seasoned, highly skilled, and adaptable full stack software
+				class: "lfw",
+				
+				r#"I am also currently looking for full time employment. If you
+				could use a seasoned, highly skilled, and adaptable software
 				developer, send me a description of the position you are looking
 				to fill. Note: I am only considering fully remote positions at
 				this time."#
@@ -57,8 +68,7 @@ fn Collaborate(cx: Scope, lookingForWork: bool) -> Element
 			
 			p
 			{
-				r#"In any case, if you're interested in working with me, feel
-				free to reach out!"#
+				r#"In any case, if you could use my help, feel free to reach out!"#
 			}
 			
 			a
@@ -71,64 +81,59 @@ fn Collaborate(cx: Scope, lookingForWork: bool) -> Element
 	});
 }
 
-// --------------------------------------------------
-
-fn GeneralistPolyglot(cx: Scope) -> Element
+/**
+Why should you listen to me?
+*/
+fn Credentials(cx: Scope) -> Element
 {
 	return cx.render(rsx!
 	{
 		section
 		{
-			h1 { "What Do I Mean By Generalist Polyglot Developer?" }
+			h1 { "WHERE CAN YOU SEE MY PORTFOLIO?" }
 			
 			p
 			{
-				r#"I consider myself to be a polyglot. I'm not referring to my
-				conversational fluency in Norwegian, my having dabbled in 10 or
-				more other spoken languages, nor my ability to read several
-				non-Latin writing systems phonetically."#
+				r#"Honestly, you're already looking at it. This site you're
+				viewing right now is an application which I built from scratch.
+				I also created the videos and images for the scrolling panels
+				above (which is why a couple of the projects don't have any
+				unique images yet). If you haven't tried building a frontend
+				with Dioxus yet, I highly recommend it!"#
 			}
 			
 			p
 			{
-				r#"A programming language is just that: a language. It is a set
-				of grammar and syntax which provides a means of communicating
-				with a computer. As with spoken language, when I learn a new
-				programming language I intuitively bring over understanding from
-				all the other languages with which I am familiar, dramatically
-				reducing the time needed to attain fluency."#
+				r#"At the top of this page, I've listed my most prominent
+				or relevant projects. When it comes to software development, my
+				primary motivation is to help people, not making a profit. This
+				is why I prefer creating free, open source applications."#
 			}
 			
 			p
 			{
-				r#"Similarly to learning a language, gaining proficiency with
-				any new technology is a process which becomes easier the more
-				you do it. Over the course of my career as a software developer
-				I have become exceedingly efficient at this process, to the
-				point of being able to start working with a completely new-to-me
-				technology almost immediately. This is why I began referring to
-				myself as an intentional generalist."#
-			}
-			
-			p
-			{
-				r#"As a result of all of this, I know I can work with any software
-				development technology regardless of whether or not I've used it
-				before."#
+				r#"You can find the rest on my Github profile. Many of the
+				projects there were exploratory learning opportunities for me or
+				simple demonstrations of my ability to work with a wide variety
+				of technologies. A few them are very old projects that I built
+				and actually used in my professional work back in the day. But
+				taken together they represent my journey on the path of the
+				software developer."#
 			}
 		}
 	});
 }
 
-// --------------------------------------------------
-
+/**
+Personal description: education/work history/interests/etc.
+*/
 fn WhoAmI(cx: Scope) -> Element
 {
 	return cx.render(rsx!
 	{
 		section
 		{
-			h1 { "Who Am I?" }
+			h1 { "WHO AM I?" }
 			
 			p
 			{
@@ -146,20 +151,35 @@ fn WhoAmI(cx: Scope) -> Element
 			
 			p
 			{
-				r#"On my own time, I prefer to explore areas of software development
-				that are outside the domains of my professional work. Most notably,
-				I have a passion for game development. I also enjoy trying out
-				new, or new-to-me, technologies. After all, there is always more
-				to learn."#
+				r#"The breadth of my experience has taught me an important lesson:
+				It is much more important to know how to find the answer than it
+				is to already know the answer. Instead of trying to keep all the
+				documentation for every existing language, library, and tool in
+				my head all the time, I focus on knowing where to go looking
+				when I need information. This is why I say with confidence that
+				I can develop software with any technology, whether I've used it
+				before or not."#
 			}
 			
 			p
 			{
-				r#"However, everyone needs to take a step back from the grind
-				and relax from time to time. I prefer immersing myself in engaging
-				stories and interactive storytelling, whether in the form of
-				movies or TV shows, video games, or collaborative storytelling
-				sessions (i.e. TTRPGs)."#
+				r#"In my free time, I tend to indulge in my passion for stories
+				and storytelling. In addition to watching movies and shows, or
+				playing narrative-driven video games, I enjoy participating in
+				collaborative storytelling games, also known as tabletop
+				roleplaying games (TTRPGs)."#
+			}
+			
+			p
+			{
+				r#"Game development has always been a mountain I've wanted to
+				climb but, in the past, I found it to be too intimidating. That
+				is, until I had time to take a breath and reflect on my career,
+				realizing just how far I had come. At the same time, I stumbled
+				upon Godot, a completely free and open source game engine, which
+				removed the final roadblock. I've been exploring game development
+				for a few years now and plan to release at least one retail title
+				in the near future."#
 			}
 		}
 	});
