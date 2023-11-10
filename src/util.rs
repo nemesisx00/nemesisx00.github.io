86a5d9a6147278b15ea6bc4096774129d2049197
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-10 01:34:03 -0500

    Update data.rs
    Added rangeRandom() function to generate a random number, between 0 and 1, and transform it to fit an arbitrary range.

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,13 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
+use ::rand::random;
+
+pub fn rangeRandom(min: f64, max: f64) -> f64
+{
+	return transformRange(random(), 0.0, 1.0, min, max);
+}
+
 pub fn transformRange(x: f64, min1: f64, max1: f64, min2: f64, max2: f64) -> f64
 {
 	return min2 + ((max2 - min2) / (max1 - min1)) * (x - min1);
@@ -10,6 +17,17 @@ mod test
 {
 	#[allow(unused_imports)]
 	use super::*;
+	
+	#[test]
+	fn testRangeRandom()
+	{
+		let min = 25.0;
+		let max = 75.0;
+		
+		let result = rangeRandom(min, max);
+		assert!(result >= 25.0);
+		assert!(result <= 75.0);
+	}
 	
 	#[test]
 	fn testTransformRange()
