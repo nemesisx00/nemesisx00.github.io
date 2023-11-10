54357847269f0c29725217da515360314c61e6d7
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-09 20:02:07 -0500

    Update rain.rs
    RainDrop and RainDropData do not need to be publicly accessible.

--- src/components/rain.rs
+++ src/components/rain.rs
@@ -21,7 +21,7 @@ pub fn RainBackground(cx: Scope) -> Element
 	});
 }
 
-pub fn RainDrop(cx: Scope) -> Element
+fn RainDrop(cx: Scope) -> Element
 {
 	let data = RainDropData::random();
 	let styles = data.toCss();
@@ -44,7 +44,7 @@ pub fn RainDrop(cx: Scope) -> Element
 const RainDropPath: &str = "M 2.5,0 C 2.6949458,3.5392017 3.344765,20.524571 4.4494577,30.9559 5.7551357,42.666753 4.5915685,50 2.5,50 0.40843152,50 -0.75513565,42.666753 0.55054234,30.9559 1.655235,20.524571 2.3050542,3.5392017 2.5,0 Z";
 
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
-pub struct RainDropData
+struct RainDropData
 {
 	pub delay: f64,
 	pub duration: f64,
